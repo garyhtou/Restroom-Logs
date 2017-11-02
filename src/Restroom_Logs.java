4557cc03fab786b997855eaf813bbd2631d60938
garytou2 <gtcowboybob@gmail.com>
2017-11-02 13:46:49 -0700

    Added Restroom Logs, removed from APCS

---
+++ src/Restroom_Logs.java
@@ -0,0 +1,44 @@
+
+
+public class Restroom_Logs {
+	/*
+	What language? 
+			If using raspberry pi, use Raspbian, code in python 
+			Arduino, c++? 
+			 
+			Signing out: 
+			Use Camera for Barcode Scanner to read barcode. 
+			Save barcode #, sign out time, add one to # of people signed out 
+			 
+			Signing in/additional sign out: 
+			Use Camera for Barcode Scanner to read barcode. 
+			If barcode matches one of the signed out, 
+			Save sign in time, subtract one from # of people signed out 
+			Else  
+			If(Check if # of signed out is less than max) { 
+			Save barcode #, sign out time, add one to # of people signed out 
+			Else 
+			Print("too many people signed out") 
+			 
+			 
+			Reset: 
+			Reset at the end of every period 
+			 
+			Max of 5 mins: 
+			If time exceeds 5 mins, turn on a led and print name and current time since sign out on screen. 
+			
+			
+			
+			
+			
+			
+			use java swing
+				use DO_NOTHING_ON_CLOSE this way the program can't be closed when you hit the closed button.
+					Maybe type in a special barcode to close program??
+			
+			
+	*/
+	
+	
+	
+}
