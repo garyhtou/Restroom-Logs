07a5152455c5f6014dfd66e2a76103bd9153a92d
Gary Tou (Home) <gtcowboybob@gmail.com>
2017-11-08 00:17:16 -0800

    added menu item with log and pdf into, worked on displaying txt file

---
+++ src/codeTest.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class codeTest {
+	public static void main(String[] args) {
+		//JOptionPane.showMessageDialog(frame, "Eggs are not supposed to be green.");
+			//frame is the component name (dataLogs)
+	}
+}
