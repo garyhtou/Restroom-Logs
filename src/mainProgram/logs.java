b34d1c830fba9666746351ca7d1f8b8d48cfc167
mschwamborn <mikems@live.com>
2017-12-08 16:25:24 -0800

    created an update query to add the TimeIn and updated the code for update pdf to match the logs db

--- src/mainProgram/logs.java
+++ src/mainProgram/logs.java
@@ -42,9 +42,24 @@ public class logs {
 	private final static String updateLogsPriority = config.updateLogsPriority;
 //FOLLOWING 3 METHODS ARE FOR INIT FOR CLEARING LOGS----------------
 	
-	
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		clearLogsDB();
+	/**
+	 * For Testing purposes only <br>
+	 * @param args
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @throws FileNotFoundException
+	 * @throws DocumentException
+	 */
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException, DocumentException {
+		//To add fields in logs db five times
+		/*for(int i =0; i<5;i++)
+			addEntryToLogDB();*/
+		//need to add intime at where studentid
+		addTimeInToLogDB("12345");
+		//then update pdf to match the db
+		updatePDF();
+		//then if needed clear the db
+		//clearLogsDB();
 	}
 //CREATE PDF FOR INIT
 	public static void createPDF() { //create blank PDF and Logs.txt for when the program initially starts up
@@ -133,10 +148,10 @@ public class logs {
 //FOLLOWING METHODS ARE FOR UPDATING PDF OR LOG-----------------------------
 
 //UDPATE PDF, call createViewPDF for viewpdf
-	public static void updatePDF() throws ClassNotFoundException, SQLException { 
+	public static void updatePDF() throws ClassNotFoundException, SQLException, DocumentException, FileNotFoundException { 
 		Document document = new Document(PageSize.LETTER, 36, 36, 60, 36);
 		
-		try {
+		
 			//creates a pdf writer with the name "testPDF.pdf" and makes it an outputable file
 			PdfWriter writer =PdfWriter.getInstance(document, new FileOutputStream(PdfLogPath));
 			PdfWriter writerCopy =PdfWriter.getInstance(document, new FileOutputStream(PdfLogViewPath));
@@ -146,38 +161,37 @@ public class logs {
 			//needs to open the document
 			document.open();
 			//Adds a new paragraph to the pdf
-			/*document.add(new Paragraph("Restroom Logs  - PDF Logs"));
-			document.add(new Paragraph(""));
-			document.add(new Paragraph("This table shows student name with entry and exit time"));*/
 			document.add(new Paragraph(" "));
 			//created a new table with 3 columns to add to the pdf
-			PdfPTable table= new PdfPTable(4);
+			PdfPTable table= new PdfPTable(5);
 			//sets the width percentage
 			table.setWidthPercentage(105);
 			//padding
 			table.setSpacingBefore(1f);
 			table.setSpacingAfter(11f);
 			//width for each column
-			float[] columnWidth= {1f,1f,0.5f,0.5f};
+			float[] columnWidth= {0.5f,1f,1f,0.5f,0.5f};
 			table.setWidths(columnWidth);
 			//sets variables for each cell
-			PdfPCell c1=new PdfPCell(new Paragraph("First Name"));
+			PdfPCell c1=new PdfPCell(new Paragraph("Student ID"));
 			c1.setBackgroundColor(BaseColor.GRAY);
-			PdfPCell c2=new PdfPCell(new Paragraph("Last Name"));
+			PdfPCell c2=new PdfPCell(new Paragraph("First name"));
 			c2.setBackgroundColor(BaseColor.GRAY);
-			PdfPCell c3=new PdfPCell(new Paragraph("Time Out"));
+			PdfPCell c3=new PdfPCell(new Paragraph("Last Name"));
 			c3.setBackgroundColor(BaseColor.GRAY);
-			PdfPCell c4=new PdfPCell(new Paragraph("Time In"));
+			PdfPCell c4=new PdfPCell(new Paragraph("Time Out"));
 			c4.setBackgroundColor(BaseColor.GRAY);
+			PdfPCell c5=new PdfPCell(new Paragraph("Time In"));
+			c5.setBackgroundColor(BaseColor.GRAY);
 			table.addCell(c1);
 			table.addCell(c2);
 			table.addCell(c3);
 			table.addCell(c4);
-			//every 3 cells is a new row
+			table.addCell(c5);
 			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
 
 			Connection conn=DriverManager.getConnection(
-		        "jdbc:ucanaccess://"+StudentDBPath);
+		        "jdbc:ucanaccess://"+LogsDBPath);
 
 		Statement s;
 		 
@@ -185,47 +199,34 @@ public class logs {
 			s = conn.createStatement();
 
 		ResultSet rs;
-		String studentID = "12345";
-		rs = s.executeQuery("SELECT [StudentID], [FirstName], [LastName] FROM ["+config.DBTableName+"]  WHERE [StudentID]='"+ studentID+"'");
+		
+		rs = s.executeQuery("SELECT [StudentID], [FirstName], [LastName], [TimeOut], [TimeIn] FROM ["+config.LogsDBTableName+"]");
 			rs.next();
-			rs.getString(1);
-			table.addCell(rs.getString(2));
+			table.addCell(rs.getString(1));
+			table.addCell(rs.getString(2));			
 			table.addCell(rs.getString(3));
-			table.addCell("   ");
-			table.addCell("sjsj");
-			
+			table.addCell(rs.getString(4));			
+			table.addCell(rs.getString(5));			
 			while (rs.next()) {
+				table.addCell(rs.getString(1));
 				table.addCell(rs.getString(2));
 				table.addCell(rs.getString(3));
+				table.addCell(rs.getString(4));
+				table.addCell(rs.getString(5));
 				}
 				
-			    
-			    
-	
-			
-			
-			
-			for(int i=0;i<3;i++) {
-			
-			}
 			
 			//adds the table on to the document
 			document.add(table);
 
-		
 			//has to end by closing the document
 			document.close();
 			
 			writerCopy.close();
 			writer.close();
-		}
-		//Error stuff so the code doesn't break
-		catch(DocumentException e) {
-			e.printStackTrace();
-		}
-		catch(FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		
+
+	
 		
 	}
 	/**
@@ -234,8 +235,7 @@ public class logs {
 	 * @throws SQLException
 	 */
 	public static void addEntryToLogDB() throws ClassNotFoundException, SQLException {
-	Document document = new Document(PageSize.LETTER, 36, 36, 60, 36);
-		//creates a pdf writer with the name "testPDF.pdf" and makes it an outputable file
+	
 		Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
 		
 		Connection conn=DriverManager.getConnection("jdbc:ucanaccess://"+LogsDBPath);
@@ -250,7 +250,7 @@ public class logs {
 		st.setString(2, "Michael");
 		st.setString(3, "Schwamborn");
 		st.setString(4, "10:55");
-		st.setString(5, "10:58");
+		st.setString(5, "");
 		st.executeUpdate();
 	}
 	/**
@@ -261,23 +261,20 @@ public class logs {
 	 */
 	//FIXME: NOT DONE
 	public static void addStudentToDB() throws ClassNotFoundException, SQLException {
-		Document document = new Document(PageSize.LETTER, 36, 36, 60, 36);
-			//creates a pdf writer with the name "testPDF.pdf" and makes it an outputable file
+		
 			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
 			
-			Connection conn=DriverManager.getConnection("jdbc:ucanaccess://"+LogsDBPath);
+			Connection conn=DriverManager.getConnection("jdbc:ucanaccess://"+StudentDBPath);
 			
 			Statement s;
 			s = conn.createStatement();
 
 			ResultSet rs;
-			String q = "INSERT INTO "+LogsTableName+" ([StudentID], [FirstName], [LastName], [TimeOut], [TimeIn]) VALUES (?, ?, ?, ?, ?)";
+			String q = "INSERT INTO "+DBTableName+" ([StudentID], [FirstName], [LastName]) VALUES (?, ?, ?)";
 			PreparedStatement st = conn.prepareStatement (q);
 			st.setString(1, "12345");
 			st.setString(2, "Michael");
 			st.setString(3, "Schwamborn");
-			st.setString(4, "10:55");
-			st.setString(5, "10:58");
 			st.executeUpdate();
 		}
 	/**
@@ -298,6 +295,28 @@ public class logs {
 	PreparedStatement st = conn.prepareStatement (q);
 	st.executeUpdate();
 	}
+	/**
+	 *  Adds the time a student comes back to Logs Database<br>
+	 * @param studentID
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public static void addTimeInToLogDB(String studentID) throws ClassNotFoundException, SQLException {
+Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+		
+		Connection conn=DriverManager.getConnection("jdbc:ucanaccess://"+LogsDBPath);
+		
+		Statement s;
+		s = conn.createStatement();
+
+		ResultSet rs;
+		String Timein = "10:55";
+		String q = "UPDATE "+LogsTableName+" SET  [TimeIn]=? WHERE [StudentID]='"+studentID+"'";
+		PreparedStatement st = conn.prepareStatement (q);
+		st.setString(1, Timein);
+		st.executeUpdate();
+	}
+	
 	
 	//UPDATE LOGS
 	/**
