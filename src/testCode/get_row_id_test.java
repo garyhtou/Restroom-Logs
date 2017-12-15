e96d905921df3a5f9766fca7e43e0e8d8a2bf5ef
mschwamborn <mikems@live.com>
2017-12-14 22:37:10 -0800

    Created a text class for only updating the time in where the id is the greatest

---
+++ src/testCode/get_row_id_test.java
@@ -0,0 +1,43 @@
+package testCode;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import mainProgram.config;
+import mainProgram.logs;
+
+public class get_row_id_test {
+	
+	public static void main(String[] args) {
+		try {
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+
+			Connection conn=DriverManager.getConnection(
+		        "jdbc:ucanaccess://"+config.LogsDBPath);
+			Statement s;
+			s = conn.createStatement();
+			
+			
+			String q = "UPDATE ["+config.LogsDBTableName+"] ([TimeIn]) VALUES (?) WHERE (SELECT MAX(12345) FROM ["+config.LogsDBTableName+"]  WHERE id = 4)";
+			PreparedStatement st = conn.prepareStatement(q);
+			
+			st.setString(1, "test");
+			
+			st.executeUpdate();
+			
+			
+
+		} catch (SQLException | ClassNotFoundException e) {
+			logs.updateLogsERROR("Couldnt not access database at  " + config.StudentDBPath +".  Returning null");
+			e.printStackTrace();
+		}
+	}
+	
+	
+
+}
+
