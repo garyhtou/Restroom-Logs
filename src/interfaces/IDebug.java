a9160c811d389b7d8dedbaf73a467b05e91e280b
garytou2 <gtcowboybob@gmail.com>
2017-12-15 21:40:23 -0800

    updated IDebug interface

--- src/interfaces/IDebug.java
+++ src/interfaces/IDebug.java
@@ -25,19 +25,3 @@ public interface IDebug {
 		}
 	}
 }
-class ArrayException extends Exception{
-	ArrayException() {}
-	ArrayException(String str)
-	{
-	   super(str);
-	}
-	static void Error() {
-		try {
-	       	throw new ArrayException("Can not debug array, please use a for loop to print.");
-			}
-		catch (ArrayException e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
-	}
-}
