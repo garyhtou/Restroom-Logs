cb94b3836c9bbc219df47390fb2306f3fcf7a7dc
garytou2 <gtcowboybob@gmail.com>
2018-02-06 11:56:30 -0800

    create new test class for JLabel field resizing

---
+++ src/testCode/JLabelResize.java
@@ -0,0 +1,26 @@
+package testCode;
+
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
+public class JLabelResize {
+	public static void main(String[] args) {
+		
+	}
+	public static class content {
+		static JSplitPane split = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		static JLabel label = new JLabel();
+		public static void label() {
+			
+		}
+		public static void settings() {
+			JPanel buttonPane = new JPanel(new BorderLayout());
+				JButton small = new JButton("<");
+					small.addActionListener(new );
+				JButton big = new JButton(">");
+				
+			JSlider
+		}
+	}
+}
