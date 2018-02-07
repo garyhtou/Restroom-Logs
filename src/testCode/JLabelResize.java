3a8a468294c10e3cc6213486449961d7f3834a3c
garytou2 <gtcowboybob@gmail.com>
2018-02-07 14:03:34 -0800

    worked on test class

--- src/testCode/JLabelResize.java
+++ src/testCode/JLabelResize.java
@@ -1,45 +1,88 @@
 package testCode;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.awt.FontMetrics;
 
 import javax.swing.*;
 
 public class JLabelResize {
 	public static void main(String[] args) {
+		create();
+		interaction();
+	}
+	
+	static JFrame frame = new JFrame();
+		static JSplitPane split = new JSplitPane();
+			static JTextField field = new JTextField();
+			static JPanel fontPane = new JPanel();
+				static JButton small = new JButton();
+				static JSlider slider = new JSlider();
+				static JButton big = new JButton();
+				
+	static Font defaultFont = mainProgram.RL.scan;
+	static Font currentFont = defaultFont;
+	static float fontSize = 20f;
+	
+	
+	public static void updateFontSize() {
 		
 	}
-	public static class content {
-		static JSplitPane split = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+	
+	
+	
+	
+	
+	
+	
+	
+	//no need to touch these methods 
+	
+	public static void create() {
+		split.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		
+		frame.add(split);
+		
+		split.setTopComponent(field);
+		split.setBottomComponent(fontPane);
+		
+		fontPane.setLayout(new BorderLayout());
+		fontPane.add(small, BorderLayout.LINE_START);
+		fontPane.add(slider, BorderLayout.CENTER);
+		fontPane.add(big, BorderLayout.LINE_END);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+	public static void interaction() {
+		small.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fontSize--;
+				updateFontSize();
+			}
+		});
+		big.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fontSize++;
+				updateFontSize();
+			}
+		});
+		field.addKeyListener(new KeyListener() {
+			public void keyPressed(KeyEvent arg0) {}
+			public void keyTyped(KeyEvent arg0) {}
+			public void keyReleased(KeyEvent arg0) {
+				// TODO Auto-generated method stub
 				
-		static JLabel label = new JLabel();
-		
-		static int fontSize = 20;
-		
-		
-		public static void label() {
-			
-		}
-		public static void settings() {
-			JPanel buttonPane = new JPanel(new BorderLayout());
-				JButton small = new JButton("<");
-					small.addActionListener(new ActionListener() {
-						public void actionPerformed(ActionEvent e) {
-							fontSize--;
-							
-						}
-					});
-				JButton big = new JButton(">");
-					big.addActionListener(new ActionListener() {
-						public void actionPerformed(ActionEvent e) {
-							fontSize++;
-							
-						}
-					});
-					
-				buttonPane.add(small, BorderLayout.LINE_START);
-				buttonPane.add(big, BorderLayout.LINE_END);
-		}
+			}
+		});
 	}
 }
